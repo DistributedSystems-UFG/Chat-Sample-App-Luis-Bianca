570b077e2736fa2ca3a77e4c597e75a3b081b5f0
lguilhermebarbosa <133176331+lguilhermebarbosa@users.noreply.github.com>
2023-06-21 21:48:57 -0300

    Add files via upload

---
+++ const.py
@@ -0,0 +1,5 @@
+CHAT_SERVER_HOST = "54.172.139.149"
+CHAT_SERVER_PORT = 5678
+
+# IDs de usuários registrados com os respectivos endereços IP e números de porta
+registry = {"Alice":("3.83.151.110",5679), "Bob":("3.94.206.221",5679)}
