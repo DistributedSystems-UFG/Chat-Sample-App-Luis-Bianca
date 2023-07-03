7c0efd25efea4b70eea01a7f96d07ba5633aab0c
biancarvalhoufg <133176164+biancarvalhoufg@users.noreply.github.com>
2023-07-02 22:00:46 -0300

    group messages

--- multithreading/chatserver.py
+++ multithreading/chatserver.py
@@ -8,7 +8,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-class ClientHandler(threading.Thread): # thread to handle the client.
+class ClientThread(threading.Thread): # thread to handle the client.
   def __init__(self, conn, addr):
     threading.Thread.__init__(self)
     self.client_conn = conn
@@ -23,41 +23,33 @@ class ClientHandler(threading.Thread): # thread to handle the client.
     src = msg_pack[2]
     logging.info("RELAYING MSG: " + msg + " - FROM: " + src + " - TO: " + dest)
 
-    try:
-      dest_addr = const.registry[dest]
-    except KeyError:
-      self.client_conn.send(pickle.dumps("NACK"))
-      self.client_conn.close()
-      logging.warning("Client %s: Destination does not exist", self.client_addr)
-      return
-
-    self.client_conn.send(pickle.dumps("ACK"))
-    self.client_conn.close()
-
-    client_sock = socket(AF_INET, SOCK_STREAM)
-    dest_ip = dest_addr[0]
-    dest_port = dest_addr[1]
-
-    try:
-      client_sock.connect((dest_ip, dest_port))
-    except:
-      logging.error()
-      logging.error("Client %s: Destination client is down", self.client_addr)
-      client_sock.close()
-      return
-
-    msg_pack = (msg, src)
-    marshaled_msg_pack = pickle.dumps(msg_pack)
-    client_sock.send(marshaled_msg_pack)
-    marshaled_reply = client_sock.recv(1024)
-    reply = pickle.loads(marshaled_reply)
-    if reply != "ACK":
-      logging.error("Client %s: Destination client did not receive message properly", self.client_addr)
+    if dest == "ALL":
+      broadcast_message(src, marshaled_msg_pack)
     else:
-      pass
-    client_sock.close()
-    
-    logging.info("Thread ended for client: %s", self.client_addr)
+      if dest in connected_clients:
+        dest_conn = connected_clients[dest]
+        dest_conn.send(marshaled_msg_pack)
+      else:
+        self.conn.send(pickle.dumps("NACK"))
+        logging.error("Client %s: Destination client is down", self.client_addr)
+
+    self.conn.close()
+    remove_client(self.conn)
+
+def broadcast_message(sender, message):
+  for conn in connected_clients.values():
+    if conn != sender:
+      conn.send(message)
+
+def remove_client(conn):
+  username = None
+  for user, client_conn in connected_clients.items():
+    if client_conn == conn:
+      username = user
+      break
+
+  if username:
+    del connected_clients[username]
 
 server_sock = socket(AF_INET, SOCK_STREAM)
 server_sock.bind(('0.0.0.0', const.CHAT_SERVER_PORT))
@@ -65,7 +57,11 @@ server_sock.listen(5)
 
 logging.info("Chat Server is ready...")
 
+connected_clients = {}
+
 while True:
-  (conn, addr) = server_sock.accept() # When a new client connection is accepted,
-  client_handler = ClientHandler(conn, addr) # a new ClientHandler thread is created
-  client_handler.start()
+  (conn, addr) = server_sock.accept()
+  client_thread = ClientThread(conn, addr)
+  client_thread.start()
+  username = client_thread.getName()
+  connected_clients[username] = conn
--- multithreading/const.py
+++ multithreading/const.py
@@ -2,4 +2,4 @@ CHAT_SERVER_HOST = "172.31.90.224"
 CHAT_SERVER_PORT = 5678
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("44.211.43.1",5679), "Bob":("3.91.188.154",5679)}
+registry = {"Alice":("44.211.43.1",5679), "Bob":("3.91.188.154",5679), "Mary":("44.201.250.75",5679)}
